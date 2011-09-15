732351fd7ed5f7be4460fa4b7aa4452a6222e4f2
benoit richard <benowa.desu@gmail.com>
2011-09-15 00:30:09 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Equipeimmobiliere::Application.config.secret_token = '405bffe3dcbd9dcad6a71de9c78f153ea178a4f8bad7fd9b1f2f04622df8bcaf286c86162159d49e0404b7dd9f8af726d9b6bbe590ba8e7afee5bafa3aa59596'
