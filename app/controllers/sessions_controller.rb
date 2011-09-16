0471b3d556eddbc85e776b3762ac68f6a97b6b83
benoit richard <benowa.desu@gmail.com>
2011-09-15 20:20:11 -0400

    some tweeak links

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to finance_url, notice: "Logged out"
+    redirect_to admin_url, notice: "Logged out"
   end
 
 end
